e742923cf7aa1453aed3a5e85402d2221113a3b0
vinai <vinai@netzarbeiter.com>
2010-06-24 10:01:34 +0200

    Add OSL-3 License Header

--- app/code/community/Saferpay/Standard/Model/Ideal.php
+++ app/code/community/Saferpay/Standard/Model/Ideal.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Saferpay Standard Magento Payment Extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Saferpay Business to
+ * newer versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @copyright Copyright (c) 2010 Openstream Internet Solutions, Switzerland
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Saferpay_Standard_Model_Ideal extends Saferpay_Standard_Model_Abstract
 {
