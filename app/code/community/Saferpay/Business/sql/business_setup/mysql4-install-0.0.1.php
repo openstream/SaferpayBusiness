ac2e8438f2af6aa54e2d46bf5bc7b05058d5a03d
Tim Bezhashvyly <tim@openstream.ch>
2012-05-14 16:23:26 +0200

    Installer for Magento 1.5+ added (Issue #7 fixed).

---
+++ app/code/community/Saferpay/Business/sql/business_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,28 @@
+<?php
+
+$magentoVersion = Mage::getVersionInfo();
+if($magentoVersion['major'] < 2 && $magentoVersion['minor'] < 5){
+
+    // Installer is not needed for versions older then 1.5.0.0
+
+}else{
+
+    $installer = $this;
+    /* @var $installer Mage_Core_Model_Resource_Setup */
+
+    $statusTable        = $installer->getTable('sales/order_status');
+    $statusStateTable   = $installer->getTable('sales/order_status_state');
+    $statusLabelTable   = $installer->getTable('sales/order_status_label');
+
+    $data = array(
+        array('status' => 'authorized', 'label' => 'Authorized Payment'),
+        array('status' => 'canceled_saferpay', 'label' => 'Canceled by Saferpay')
+    );
+    $installer->getConnection()->insertArray($statusTable, array('status', 'label'), $data);
+
+    $data = array(
+        array('status' => 'authorized', 'state' => 'authorized', 'is_default' => 1),
+        array('status' => 'canceled_saferpay', 'state' => 'canceled_saferpay', 'is_default' => 1)
+    );
+    $installer->getConnection()->insertArray($statusStateTable, array('status', 'state', 'is_default'), $data);
+}
